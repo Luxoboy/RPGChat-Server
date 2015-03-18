069abeebd86e69cdff9ef421190eeb495581791f
Anthony Correia <anthony.correia71@gmail.com>
2015-03-18 09:54:44 +0100

    Documentation.

--- Server.h
+++ Server.h
@@ -16,17 +16,27 @@
 class Server
 {
 public:
+    /**
+     * Sets all the variables and creates the listening socket.
+     * Sets the READY bool.
+     */
     Server();
     virtual ~Server();
     
+    /**
+     * Tells wether the init (constructor) went well.
+     * Should be called after constructor.
+     * @return False if there was any error.
+     */
     bool ready();
+    
 private:
-    bool READY;
+    bool READY; //Indicates wether everything went well in construtor.
     int status; //Status variable
     int bind_socketfb; //Socket descriptor of listening socket.
     struct addrinfo host_info;       // The struct that getaddrinfo() fills up with data.
     struct addrinfo *host_info_list; // Pointer to the to the linked list of host_info's.
-
+    
 };
 
 #endif	/* SERVER_H */
