beb931d04eb566ac6ebc48abb8e5a8f7a8de9a0f
Anthony Correia <anthony.correia71@gmail.com>
2015-03-18 12:29:20 +0100

    Added threads. Added prefix method.

--- Client.cpp
+++ Client.cpp
@@ -8,14 +8,36 @@
 #include "Client.h"
 #include "Server.h"
 
+#include <iostream>
+
+using namespace std;
+
 Client::Client(Server *server, int id, int socket)
 {
+    cout << "[CLIENT] Entering client constructor, id = " << id << endl;
     this->id = id;
     this->socket = socket;
     this->server = server;
+    
+    cout << prefix() << "Creating reading and listening threads." << endl;
 }
 
 Client::~Client()
 {
+}
+
+void Client::_readingThread()
+{
+    
+}
+
+void Client::_writingThread()
+{
+    
+}
+
+char* Client::prefix()
+{
+    return "[Client " + id + "] ";
 }
 
--- Client.h
+++ Client.h
@@ -8,6 +8,8 @@
 #ifndef CLIENT_H
 #define	CLIENT_H
 
+#include <thread>
+
 class Server;
 
 
@@ -20,6 +22,16 @@ private:
     int id;
     int socket;
     Server *server;
+    std::thread *readingThread, *writingThread;
+    void _readingThread();
+    void _writingThread();
+    
+    /**
+     * Helper methods which returns output prefix. 
+     * Exemple : [Client 1]
+     * @return The string to be put at the begining of the output line.
+     */
+    virtual char* prefix();
 };
 
 #endif	/* CLIENT_H */
