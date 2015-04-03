a8523178a3915c0120ccdcb1072955d730973346
Anthony Correia <anthony.correia71@gmail.com>
2015-04-03 23:53:45 +0200

    Working main.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 
 #include <cstdlib>
 
+#include "Server.h"
+#include <iostream>
 using namespace std;
 
 /*
@@ -14,7 +16,10 @@ using namespace std;
  */
 int main(int argc, char** argv)
 {
-
+    Server s1;
+    if(s1.ready())
+        s1.accept();
+    
     return 0;
 }
 
